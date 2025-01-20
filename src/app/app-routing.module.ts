5577153f8c1af58c07e95ba9749e55aaf13941c7
Shruthi Kulkarni <shruthip15.sk@gmail.com>
2025-01-20 15:13:42 +0000

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
-=======
->>>>>>> 14b885770bb29a3543906d741567130c7d004272
 import { MenuComponent } from './menu/menu.component';
 
 const routes: Routes = [{ path: 'home', component: HomeComponent },
@@ -17,9 +14,6 @@ const routes: Routes = [{ path: 'home', component: HomeComponent },
 <<<<<<< HEAD
                         { path: 'contact', component: ContactComponent },
                         { path: 'login', component: LoginComponent}];
-=======
-                        { path: 'contact', component: ContactComponent }];
->>>>>>> 14b885770bb29a3543906d741567130c7d004272
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
